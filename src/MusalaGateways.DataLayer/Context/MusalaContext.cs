3ed57fe79d16223129d8ad7bfe1e4eeaab2888e4
Lexander <lexog26@gmail.com>
2020-12-18 09:26:56 -0500

    MusalaContext added

---
+++ src/MusalaGateways.DataLayer/Context/MusalaContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MusalaGateways.DataLayer.EntityConfigs;
+using MusalaGateways.Domain.Entities;
+using System.IO;
+
+namespace SurgerySimulator.DataLayer.Context
+{
+    public partial class MusalaContext : DbContext
+    {
+        public MusalaContext(DbContextOptions<MusalaContext> options) : base(options)
+        { }
+
+        public MusalaContext() : base()
+        { }
+
+        #region DbSets
+
+        public DbSet<Gateway> Gateways { get; set; }
+
+        public DbSet<Device> Devices { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            new GatewayConfig(builder.Entity<Gateway>());
+            new DeviceConfig(builder.Entity<Device>());
+        }
+
+    }
+}
