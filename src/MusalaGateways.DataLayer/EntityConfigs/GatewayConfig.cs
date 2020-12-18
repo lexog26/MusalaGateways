636ae6dcd98827db32b97f9673df441c8c24478f
Lexander <lexog26@gmail.com>
2020-12-17 22:19:16 -0500

    Gateway entity configuration added

---
+++ src/MusalaGateways.DataLayer/EntityConfigs/GatewayConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusalaGateways.Domain.Entities;
+
+namespace MusalaGateways.DataLayer.EntityConfigs
+{
+    public class GatewayConfig
+    {
+        public GatewayConfig(EntityTypeBuilder<Gateway> entity)
+        {
+            entity.ToTable("Gateways");
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Id).ValueGeneratedOnAdd();
+            entity.Property(p => p.Name).IsRequired(false).HasMaxLength(50);
+            entity.Property(p => p.SerialNumber).IsRequired().HasMaxLength(255);
+            entity.Property(p => p.Ipv4Address).IsRequired().HasMaxLength(255);
+
+            entity.Property(p => p.LastTimeStamp).IsRequired().ValueGeneratedOnAddOrUpdate();
+            entity.Property(p => p.ModifiedDate).IsRequired(false).ValueGeneratedOnUpdate();
+            entity.Property(p => p.CreatedDate).IsRequired().ValueGeneratedOnAdd();
+
+            entity.HasIndex(p => p.SerialNumber).IsUnique();
+
+            entity.HasMany(p => p.Devices)
+                  .WithOne(d => d.Gateway);
+
+        }
+    }
+}
