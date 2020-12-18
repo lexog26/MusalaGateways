443a657bb309c4a3adf75a50361afec37bd0f82a
Lexander <lexog26@gmail.com>
2020-12-18 13:29:19 -0500

    Musala Mapper added

---
+++ src/MusalaGateways.BusinessLogic/Configurations/Mapper/MapperProfileBase.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace MusalaGateways.BusinessLogic.Configurations.Mapper
+{
+    /// <summary>
+    /// Provides Mappers from entities to dto
+    /// </summary>
+    public abstract class MapperProfileBase : Profile
+    {
+    }
+}
---
+++ src/MusalaGateways.BusinessLogic/Configurations/Mapper/MusalaMapperProfile.cs
@@ -0,0 +1,29 @@
+﻿using MusalaGateways.DataTransferObjects.Dtos;
+using MusalaGateways.Domain.Entities;
+using System.Linq;
+
+namespace MusalaGateways.BusinessLogic.Configurations.Mapper
+{
+    public class MusalaMapperProfile : MapperProfileBase
+    {
+        public MusalaMapperProfile()
+        {
+            CreateMapperConfig();
+        }
+
+        /// <summary>
+        /// Maps entities <---> dtos
+        /// </summary>
+        internal void CreateMapperConfig()
+        {
+            //Gateway to GatewayDto, GatewayDto to Gateway
+            CreateMap<Gateway, GatewayDto>()
+                .ForMember(dto => dto.DevicesIds, map => map.MapFrom(org => org.Devices.Select(x => x.Id)))
+                .ReverseMap();
+
+            //Device to DeviceDto, DeviceDto to Device
+            CreateMap<Device, DeviceDto>()
+                .ReverseMap();
+        }
+    }
+}
