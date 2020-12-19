cf764eda05e6923132a40e1019976f440478f44d
Lexander <lexog26@gmail.com>
2020-12-18 21:41:49 -0500

    Initial migration created

---
+++ src/MusalaGateways.DataLayer/Migrations/20201219024100_Initial_Create.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusalaGateways.DataLayer.Migrations
+{
+    public partial class Initial_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gateways",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    LastTimeStamp = table.Column<DateTime>(nullable: false),
+                    SerialNumber = table.Column<string>(maxLength: 255, nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    Ipv4Address = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gateways", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    ModifiedDate = table.Column<DateTime>(nullable: true),
+                    LastTimeStamp = table.Column<DateTime>(nullable: false),
+                    Uid = table.Column<int>(nullable: false),
+                    Vendor = table.Column<string>(maxLength: 100, nullable: true),
+                    Status = table.Column<int>(nullable: false, defaultValue: 0),
+                    GatewayId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Devices_Gateways_GatewayId",
+                        column: x => x.GatewayId,
+                        principalTable: "Gateways",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_GatewayId",
+                table: "Devices",
+                column: "GatewayId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gateways_SerialNumber",
+                table: "Gateways",
+                column: "SerialNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Gateways");
+        }
+    }
+}
